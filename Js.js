d02a3c9c87e28a94243aca3023af056cbcda6daf
xTiiX <arthur.nordmann@supinfo.com>
2020-03-12 11:52:17 +0100

    Test ForFor RefreshScreen

--- Js.js
+++ Js.js
@@ -1,3 +1,23 @@
-let a = 3;
-if (a == 2) console.log("A = 2");
-if (a == 3) console.log("A = 3");
+/*Maze Templates*/
+var maze5 = [[0,0,0,3,0],
+            [1,1,0,1,0],
+            [0,1,1,1,0],
+            [1,1,0,1,1],
+            [1,0,0,0,0]]
+
+var canvas = document.getElementById("Board");
+
+function refreshScreen(){
+    var context = canvas.context("2d");
+    var width = canvas.width;
+    var height = canvas.height;
+
+    for (let y = 0; y < mase5.length; y++) {
+        for (let x = 0; x < mase5[y].length; x++) {
+            context.fillStyle = red;
+            context.fillRect(x, y, width/mase5[y].length, height/mase5.length);
+        }
+        
+    }
+
+}
