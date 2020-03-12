93570cde6a6ee5ba07a3d17664b9e77f0e8d3e68
xTiiX <arthur.nordmann@supinfo.com>
2020-03-12 13:43:09 +0100

    Complete maze refresh

--- Js.js
+++ Js.js
@@ -5,17 +5,34 @@ var maze5 = [[0,0,0,3,0],
             [1,1,0,1,1],
             [1,0,0,0,0]]
 
-var canvas = document.getElementById("Board");
+var canvas = document.getElementById("board");
 
 function refreshScreen(){
-    var context = canvas.context("2d");
+    var context = canvas.getContext("2d");
     var width = canvas.width;
     var height = canvas.height;
+    var boxSize = width / maze5.length;
+    console.log("Kebab")
 
-    for (let y = 0; y < mase5.length; y++) {
-        for (let x = 0; x < mase5[y].length; x++) {
-            context.fillStyle = red;
-            context.fillRect(x, y, width/mase5[y].length, height/mase5.length);
+    for (let y = 0; y < maze5.length; y++) {
+        for (let x = 0; x < maze5[y].length; x++) {
+            if (maze5[y][x] == 0) {
+                context.fillStyle = "black";
+                context.fillRect(x*boxSize, y*boxSize, boxSize, boxSize);
+            }
+            else if (maze5[y][x] == 1) {
+                context.fillStyle = "white";
+                context.fillRect(x*boxSize, y*boxSize, boxSize, boxSize);
+            }
+            else if (maze5[y][x] == 2) {
+                context.fillStyle = "white";
+                context.fillRect(x*boxSize, y*boxSize, boxSize, boxSize);
+                
+            }
+            else if (maze5[y][x] == 3) {
+                context.fillStyle = "red";
+                context.fillRect(x*boxSize, y*boxSize, boxSize, boxSize);
+            }
         }
         
     }
